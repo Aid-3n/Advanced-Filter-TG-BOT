3215bd5a6d92fcf0fed72ab1a90170c88a475991
Jason-Brody <65779952+Jason-Brody@users.noreply.github.com>
2020-08-17 21:13:46 +0530

    fix

--- cinderella/modules/cust_filters.py
+++ cinderella/modules/cust_filters.py
@@ -289,7 +289,7 @@ def stop_all_filters(bot: Bot, update: Update):
     
     
 def __stats__():
-    return "{} filters, across {} chats.".format(sql.num_filters(), sql.num_chats())
+    return "{}* Filters* *Across* {} chats.".format(sql.num_filters(), sql.num_chats())
 
 
 def __migrate__(old_chat_id, new_chat_id):
